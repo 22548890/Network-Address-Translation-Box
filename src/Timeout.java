cf974ea6306ea58d52f021a7b3e135e1ba71edd0
Sass, MF, Mr [22548890@sun.ac.za] <22548890@sun.ac.za>
2022-08-31 09:42:36 +0200

    Update src/Timeout.java

--- src/Timeout.java
+++ src/Timeout.java
@@ -1,8 +1,5 @@
 /**
- * Returns the current state of the player's stream.
- * 
- * @param player The player whose stream state is being requested.
- * @return The current state of the player's stream.
+ * implements a TIMEOUT in mins to disconnect a client if inactive
  */
 public class Timeout implements Runnable {
     static final int TIMEOUT = 15 * (1000 * 60);// 15mins
@@ -12,8 +9,7 @@ public class Timeout implements Runnable {
     }
 
     /**
-     * The timeout thread that will be used to kill the server if it doesn't
-     * shutdown properly.
+     * The timeout thread that will be used to kill the server when expired
      */
     @Override
     public void run() {
